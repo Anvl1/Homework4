247514f74ba23d3debd71e46c093888af47828ed
Anvl1 <88466348+Anvl1@users.noreply.github.com>
2025-05-26 08:39:59 +1000

    Add files via upload

---
+++ Homework4.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main() 
+{
+    int num;
+    int sum = 0;
+    
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "rus");
+
+    do 
+    {
+        std::cout << "Введите целое число или число '0', чтобы закончить: ";
+        std::cin >> num;
+
+        if (num != 0) 
+        {
+            sum += num;
+        }
+    } 
+    while (num != 0);
+
+    std::cout << "Сумма: " << sum << std::endl;
+    return 0;
+}
---
+++ Homework4.2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "rus");
+    int num;
+    std::cout << "Введите целое число: ";
+    std::cin >> num;
+
+    if (num < 0) 
+    {
+        num = -num;
+    }
+
+    int sum = 0;
+
+    while (num > 0) 
+    {
+        sum += num % 10; 
+        num /= 10;
+    }
+
+    std::cout << "Сумма цифр: " << sum << std::endl;
+
+    return 0;
+}
