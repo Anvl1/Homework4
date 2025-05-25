65ab173689304ba17d038ce7a3ba5bdaa32a58a4
Anvl1 <88466348+Anvl1@users.noreply.github.com>
2025-05-26 09:20:49 +1000

    Add files via upload

---
+++ Homework4.3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "rus");
+    int num;
+
+    std::cout << "Введите целое число: ";
+    std::cin >> num;
+
+    for (int i = 1; i <= 10; ++i) {
+        std::cout << num << " x " << i << " = " << num * i << std::endl;
+    }
+    return 0;
+}
